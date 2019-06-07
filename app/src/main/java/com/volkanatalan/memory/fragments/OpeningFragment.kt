981c419faa1afe22fb641e0067ae5489ec343085
Volkan ATALAN <volkanatalan35@gmail.com>
2019-06-08 02:38:06 +0300

    Minor change in into animation

--- app/src/main/java/com/volkanatalan/memory/fragments/OpeningFragment.kt
+++ app/src/main/java/com/volkanatalan/memory/fragments/OpeningFragment.kt
@@ -3,13 +3,16 @@ package com.volkanatalan.memory.fragments
 
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.FrameLayout
 import androidx.core.animation.doOnEnd
 import androidx.fragment.app.Fragment
+import androidx.vectordrawable.graphics.drawable.ArgbEvaluator
 import com.volkanatalan.memory.R
 import com.volkanatalan.memory.activities.MainActivity
 import kotlinx.android.synthetic.main.fragment_opening.view.*
@@ -20,6 +23,8 @@ class OpeningFragment : Fragment() {
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
     // Inflate the layout for this fragment
     val view = inflater.inflate(R.layout.fragment_opening, container, false)
+    val base = view.base as FrameLayout
+    
     val density = resources.displayMetrics.density
     val mLetterX = view.m_letter.x
     val mLetterToX = mLetterX - (80 * density)
@@ -46,13 +51,22 @@ class OpeningFragment : Fragment() {
   
     val brainScaleYAnimator = ObjectAnimator.ofFloat(view.brain, "scaleY", 10f)
     brainScaleYAnimator.duration = 500
-    
+  
+    val colorFrom = resources.getColor(R.color.colorPrimaryDark)
+    val colorTo = resources.getColor(R.color.white)
+    val colorAnimation = ValueAnimator.ofObject(ArgbEvaluator(), colorFrom, colorTo)
+    colorAnimation.duration = 500
+    colorAnimation.addUpdateListener {
+      base.setBackgroundColor(it.animatedValue as Int)
+    }
+  
     val animSet = AnimatorSet()
     animSet.play(mLetterTranslateAnimator).with(brainAlphaAnimator)
     animSet.play(mLetterTranslateAnimator).with(textAlphaAnimator)
     animSet.play(mLetterAlphaAnimator).before(mLetterTranslateAnimator)
     animSet.play(mLetterTranslateAnimator).before(brainScaleXAnimator)
     animSet.play(brainScaleXAnimator).with(brainScaleYAnimator)
+    animSet.play(brainScaleXAnimator).with(colorAnimation)
     animSet.doOnEnd {
       val mainActivity = activity as MainActivity
       mainActivity.onBackPressed()
